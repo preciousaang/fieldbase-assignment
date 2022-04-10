9ba00ceb06db5adc7ac08b5006973052d424154a
Precious Ibeagi <preciousaang@gmail.com>
2022-04-10 16:12:53 +0100

    improved styles a bit

--- fieldbase-frontend/src/App.js
+++ fieldbase-frontend/src/App.js
@@ -3,18 +3,19 @@ import { Routes, Route, Link } from "react-router-dom";
 import AddGateway from "./pages/AddGateway";
 import SingleGateway from "./pages/SingleGateway";
 import Gateways from "./pages/Gateways";
+import { NavBar } from "./shared/styledComps";
 
 function App() {
   return (
     <div className="App">
-      <ul>
+      <NavBar>
         <li>
           <Link to="/gateways">List gateways</Link>
         </li>
         <li>
           <Link to="/add-gateway">Add Gateway</Link>
         </li>
-      </ul>
+      </NavBar>
       <Routes>
         <Route path="/add-gateway" element={<AddGateway />} />
         <Route path="/gateway/:id" element={<SingleGateway />} />
--- fieldbase-frontend/src/shared/styledComps.js
+++ fieldbase-frontend/src/shared/styledComps.js
@@ -13,7 +13,7 @@ export const Error = styled.p`
 `;
 
 export const Header = styled.h2`
-  color: red;
+  color: black;
 `;
 
 export const ResponsiveTable = styled.div`
@@ -23,14 +23,38 @@ export const ResponsiveTable = styled.div`
 export const IField = styled(Field)``;
 
 export const Table = styled.table`
-  border: 1px solid black;
+  border: 1px solid gray;
+  border-radius: 4px;
+  margin: 5px auto;
+
   tbody > tr > td {
-    border: 1px solid black;
+    border: 1px solid gray;
   }
   tbody > tr > td:first-child {
     font-weight: bold;
+    padding: 5px;
   }
   thead > tr > th {
-    border: 1px solid black;
+    border: 1px solid gray;
+    padding: 10px;
+  }
+`;
+export const NavBar = styled.ul`
+  li {
+    display: inline;
+    margin: 0 5px;
+  }
+  li a {
+    text-decoration: none;
+    background-color: teal;
+    color: white;
+    padding: 5px;
+    border-radius: 5px;
+    transition: 500ms;
+    &:hover {
+      background-color: white;
+      color: teal;
+      transition: 500ms;
+    }
   }
 `;
